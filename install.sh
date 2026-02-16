511b14ff34514ffd095237acbc9b617b9ee50d45
Zak Stam <zakscomputers@hotmail.com>
2026-02-16 16:24:46 +0200

    Make dist/index.js executable after build

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ git clone --depth 1 "$REPO" "$INSTALL_DIR"
 cd "$INSTALL_DIR"
 npm install --ignore-scripts
 npm run build
+chmod +x dist/index.js
 
 # Link globally (may need sudo)
 npm link 2>/dev/null || sudo npm link
